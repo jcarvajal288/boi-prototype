ce3c48f0364684d420a1194c1bfa4b286c3563a4
Max Carvajal <jcarvajal288@gmail.com>
2026-09-02 07:53:47 -0500

    Add single shot power up to level

---
+++ Items/FacingShotSingle/facing_shot_single.gd
@@ -0,0 +1,11 @@
+extends Sprite2D
+
+
+func _ready() -> void:
+	$Area2D.body_entered.connect(on_body_entered)
+
+
+func on_body_entered(body: Node2D) -> void:
+	if body is Player:
+		print("Single Shot Facing picked up")
+		queue_free()
