2f30d988cda098e8e0cd6712207908d3638cfd80
Max Carvajal <jcarvajal288@gmail.com>
2026-09-03 08:29:13 -0500

    Put single shot data in a resource

--- Items/FacingShotSingle/facing_shot_single.gd
+++ Items/FacingShotSingle/facing_shot_single.gd
@@ -1,11 +1,14 @@
-extends Sprite2D
+extends Area2D
+
+@export var item_data: EquippableItemData
 
 
 func _ready() -> void:
-	$Area2D.body_entered.connect(on_body_entered)
+	body_entered.connect(on_body_entered)
+	$Sprite2D.texture = item_data.image
 
 
 func on_body_entered(body: Node2D) -> void:
 	if body is Player:
-		print("Single Shot Facing picked up")
+		print("Facing Shot Single picked up")
 		queue_free()
---
+++ Items/equippable_item_data.gd
@@ -0,0 +1,5 @@
+class_name EquippableItemData extends Resource
+
+@export var name: String = "<Placeholder Item Name"
+@export var description: String = "<Placeholder Item Description"
+@export var image: Texture2D
