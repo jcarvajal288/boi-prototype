2e588d552ed550c426b59d02c8f35c139101c910
Max Carvajal <jcarvajal288@gmail.com>
2026-09-16 08:13:41 -0500

    Move new item to A or B slot

--- UI/WeaponSelectionScreen/weapon_selection_screen.gd
+++ UI/WeaponSelectionScreen/weapon_selection_screen.gd
@@ -5,3 +5,14 @@ var new_item: EquippableItemData
 
 func _ready() -> void:
 	$NewItemSprite.texture = new_item.image
+
+
+func _process(_delta: float) -> void:
+	if (Input.is_action_just_pressed("b_button")):
+		$NewItemSprite.texture = null
+		$BItemSprite.texture = new_item.image
+		$AItemSprite.texture = null
+	elif (Input.is_action_just_pressed("a_button")):
+		$NewItemSprite.texture = null
+		$BItemSprite.texture = null
+		$AItemSprite.texture = new_item.image
