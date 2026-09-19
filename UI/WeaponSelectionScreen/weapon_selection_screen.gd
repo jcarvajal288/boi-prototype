7db440510045b0d5a1fbfe65ba532a4358713c03
Max Carvajal <jcarvajal288@gmail.com>
2026-09-19 14:05:48 -0500

    Quit weapon select screen on double button press

--- UI/WeaponSelectionScreen/weapon_selection_screen.gd
+++ UI/WeaponSelectionScreen/weapon_selection_screen.gd
@@ -2,17 +2,31 @@ extends CanvasLayer
 
 var new_item: EquippableItemData
 
+var moved_to_b_slot = false
+var moved_to_a_slot = false
+
 
 func _ready() -> void:
 	$NewItemSprite.texture = new_item.image
 
+# TODO: player is still active while this is happening. pause game
 
 func _process(_delta: float) -> void:
 	if (Input.is_action_just_pressed("b_button")):
-		$NewItemSprite.texture = null
-		$BItemSprite.texture = new_item.image
-		$AItemSprite.texture = null
+		if moved_to_b_slot:
+			queue_free()
+		else:
+			$NewItemSprite.texture = null
+			$BItemSprite.texture = new_item.image
+			$AItemSprite.texture = null
+			moved_to_b_slot = true
+			moved_to_a_slot = false
 	elif (Input.is_action_just_pressed("a_button")):
-		$NewItemSprite.texture = null
-		$BItemSprite.texture = null
-		$AItemSprite.texture = new_item.image
+		if moved_to_a_slot:
+			queue_free()
+		else:
+			$NewItemSprite.texture = null
+			$BItemSprite.texture = null
+			$AItemSprite.texture = new_item.image
+			moved_to_b_slot = false
+			moved_to_a_slot = true
