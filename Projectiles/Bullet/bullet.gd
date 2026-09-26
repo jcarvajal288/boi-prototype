f69a1e0b94b3a7eed189b620dbc98f9ee6984d02
Max Carvajal <jcarvajal288@gmail.com>
2026-09-26 14:43:45 -0500

    Cull bullets that hit walls or edge of screen

--- Projectiles/Bullet/bullet.gd
+++ Projectiles/Bullet/bullet.gd
@@ -3,7 +3,19 @@ extends Sprite2D
 var speed = 300
 var direction = Vector2.ZERO
 
+func _ready() -> void:
+	$Area2D.area_entered.connect(_on_area_entered)
+	$Area2D.body_entered.connect(_on_body_entered)
+
 
 func _physics_process(delta: float) -> void:
 	var displacement = speed * direction * delta
 	global_position += displacement
+
+
+func _on_area_entered(_area: Area2D) -> void:
+	queue_free()
+
+
+func _on_body_entered(_body: Node2D) -> void:
+	queue_free()
